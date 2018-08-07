cabab7455eae6e9d5121a31be03b43a008a10647
Jay Lee <eggboy@gmail.com>
2018-08-07 09:58:03 +0800

    up

--- riff.sh
+++ riff.sh
@@ -1,2 +1,2 @@
-riff service delete function-riff
+#riff service delete function-riff
 riff function create java function-riff --git-repo https://github.com/eggboy/riff-springcloudfunction.git --artifact "function-0.0.1-SNAPSHOT.jar" --handler "sayHi" --image eggboy/riff-function
