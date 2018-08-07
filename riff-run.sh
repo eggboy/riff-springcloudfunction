07665314fc3a612af16cbdc217dfaf51d36a9a24
Jay Lee <eggboy@gmail.com>
2018-08-07 09:26:50 +0800

    up

--- riff-run.sh
+++ riff-run.sh
@@ -1,4 +1,5 @@
 export SERVICE_IP=`kubectl get svc knative-ingressgateway -n istio-system -o jsonpath="{.status.loadBalancer.ingress[*].ip}"`
-
-riff service invoke function-riff  --   --header "Content-type: text/plain" -d "hello knative"
-
+echo $SERVICE_IP
+#riff service invoke function-riff  --   --header "Content-type: text/plain" -d "Jay"
+curl -w '\n'  -H 'Host: function-riff.default.example.com' -H 'Content-Type: text/plain' http://$SERVICE_IP -d "Jay"
+curl -H "Host: helloworld-go.default.example.com" http://$SERVICE_IP
